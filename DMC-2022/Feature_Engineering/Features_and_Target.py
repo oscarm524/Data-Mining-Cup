85c666906b18d05c4f1870310a94b5d8f2873e7f
Oscar <oscar.aguilar.1987@gmail.com>
2022-06-28 13:32:22 +0000

    updating libraries

--- DMC-2022/Feature_Engineering/Features_and_Target.py
+++ DMC-2022/Feature_Engineering/Features_and_Target.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
-from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score, precision_recall_curve, accuracy_score, roc_curve, confusion_matrix
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import NearestNeighbors
+
+
